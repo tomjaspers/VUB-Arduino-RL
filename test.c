033f4e0a6495435c4dd3d1bc9f6ea0314d6dacbd
Tom Jaspers <tomjaspers@gmail.com>
2014-08-28 14:32:34 +0200

    Implementation van Q-learning om de bal in het midden van het scherm te houden. Wegens geheugenbeperkingen zijn er maar 3 acties.

--- test.c
+++ test.c
@@ -20,6 +20,12 @@
 */
 #include "lib.h"
 #include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>	 
+#include <assert.h>  
+	   
+// Function prototypes
+int randint();
 
 //CONSTANTS
 static const int SIZE = 10;
@@ -27,37 +33,177 @@ static const int X_PIN = 2;
 static const int Y_PIN = 3;
 static const int STEP  = 10;
 
+/*
+
+Drawing of the board, divided in its 4 quadrants we use for our states in Q-learning (due to memory restrictions)
+
+E.g.: actions in quadrant B, state (3,1) are the same as in quadrant A for the same state, mirrored over the X-axis
+
+   0  1  2  3  4  5  6  5  4  3  2  1  0 
+0 +------------------+-----------------+
+1 |                  |                 |
+2 |                  |                 |
+3 |        A         |        B        |
+4 |                  |                 |
+5 |                  |                 |
+6 +------------------------------------+
+5 |                  |                 |
+4 |                  |                 |
+3 |        C         |        D        |
+2 |                  |                 |
+1 |                  |                 |
+0 +------------------+-----------------+
+
+*/ 
+
 void initializeBoard() {
 	USART_Init(MYUBRR);
 	initDisplay();
 	clearScreen();
 }
 
-int  main() {
+void moveBall(Ball *b, direction d, int step){
+	switch(d) {
+		case LEFT:  sendMessage(b, move, b->x_pos+step, b->y_pos); break;
+		case RIGHT: sendMessage(b, move, b->x_pos-step, b->y_pos); break;
+		case DOWN:  sendMessage(b, move, b->x_pos, b->y_pos-step); break;
+		case UP:    sendMessage(b, move, b->x_pos, b->y_pos+step); break;
+		default: break;
+	}
+}
+
+int  main() {	
+	srand(0.314);
 	initializeBoard();
 	//Create ball in the center of the screen
 	Ball* b = createBall((SCREEN_WIDTH/2)-SIZE/2,(SCREEN_HEIGHT/2)-SIZE/2, SIZE,SIZE);
+	// Draw the ball in its initial position 
+	fillRectangle(ball->x_pos, ball->y_pos, ball->width, ball->height, ball->color);
 	//Set the ball to be white
 	b->color = WHITE;
 	//Create an accelerometer connected to the X and Y_PIN 
 	Accelerometer* acc = newAccelerometer(X_PIN,Y_PIN); 
-	printNumber(23);
+	// Set the parameters for the Q-learning algorithm with epsilon-greedy action selection
+    float alpha = 0.1; // Learning rate (rate at which new training samples replace previous knowledge)
+	float gamma = 0.9; // Discount factor (defines relative values of the immediate vs delayed reward)
+	int epsilon = 10; // Exploration rate in epsilon-greedy action selection (percentage at which a random action instead of optimal is chosen)
+	
+	// Initialize our Q-values table
+	// We do not have enough memory for a simple 13 x, 13y, 5 actions mapping:
+	// 	13x13x5 = 845 floats = 3380 bytes >>> 1024 available
+	// Note that even with using ints for the float math, it would be impossible (sizeof(int) = 2)
+	float qvalues[7][7][3] = {}; // (7x7x4 floats) x 4 bytes = 784 bytes
+	int numActions = 3;
+	
 	while(1) {
 		//Ask the accelerometer for the direction
 		direction d = sendMessage(acc,getDirection);
-		switch(d) {
-			case UP:    sendMessage(b,move,b->x_pos,b->y_pos+STEP); break;
-			case DOWN:  sendMessage(b,move,b->x_pos,b->y_pos-STEP); break;
-			case LEFT:  sendMessage(b,move,b->x_pos+STEP,b->y_pos); break;
-			case RIGHT: sendMessage(b,move,b->x_pos-STEP,b->y_pos); break;
+		moveBall(b, d, STEP);
+		_delay_ms(75);	
+		
+		// Get the current state (x,y)
+		int x = b->x_pos / 10;
+		int y = b->y_pos / 10;
+		if (x < 0)	x = 0;
+		if (x > 12)	x = 12;
+		if (y < 0)	y = 0;
+		if (y > 12) y = 12;
+		x = (x > 6) ? (12 - x) : x; 		// allow to count states as 0,1,..,5,6,5,..1,0 (see drawing)
+		y = (y > 6) ? (12 - y) : y;
+		
+		// Choose an action following epsilon-greedy
+		int actionIdx;
+		if ((rand() % 101) < epsilon){ // TODO: achieve better randomness using randint(n) -> it will take memory !
+			// Choose a random action with a probability of epsilon
+			actionIdx = rand() % numActions;
+		} else {
+			// Choose the best action (= max Q-value) with probability 1-epsilon
+			// TODO: Make bias towards neutral action by counting down from i=4; i>=0; i--
+			actionIdx = 0;
+			int i;
+			for (i = 1; i < numActions; i++) {
+				if (qvalues[x][y][i] > qvalues[x][y][actionIdx]) {
+					actionIdx = i;
+				}
+			}
 		}
-	//	printNumber(b->x_pos);
-	//	USART_Transmit(':');
-///		printNumber(b->y_pos);
-//		USART_Transmit('\n');
-		_delay_ms(100);	
+		
+		direction action = (direction) actionIdx;
+		// We have to map the inverse direction depending on the quadrant we are in (default A)
+		// (see drawing)
+		if (b->x_pos > 60){ // Quadrant B / D
+			switch(action) {
+				case LEFT:  action = RIGHT; break;
+				case RIGHT: action = LEFT; break;
+				default: break;
+			}
+		}
+		if (b->y_pos > 60){ // Quadrant C / D
+			switch(action) {
+				case DOWN:  action = UP; break;
+				case UP: 	action = DOWN; break;
+				default: break;
+			}
+		}
+		
+		// Perform the action
+		moveBall(b, action, 5);
+		
+		// Get the new state (x,y)
+		int new_x = b->x_pos / 10;
+		int new_y = b->y_pos / 10;
+		if (new_x < 0)	new_x = 0;
+		if (new_x > 12)	new_x = 12;
+		if (new_y < 0)	new_y = 0;
+		if (new_y > 12) new_y = 12;
+		new_x = (new_x > 6) ? (12 - new_x) : new_x; 	// allow to count states as 0,1,..,5,6,5,..1,0 (see drawing)
+		new_y = (new_y > 6) ? (12 - new_y) : new_y;
+			
+		// Get the qvalue for the best possible action in for the new state (call e-greedy with e=0)
+		int new_action = 0;
+		int i; 
+		for (i = 0; i< numActions; i++) { 
+			if (qvalues[new_x][new_y][i] > qvalues[new_x][new_y][new_action]){
+				new_action = i;
+			}
+		}
+		
+		// Get the reward and reposition if needed
+		int reward = 0;
+		if (new_x == 6 && new_y == 6){
+			reward = 100;
+		} else if (new_x == 0  || new_y == 0 || new_x == 12 || new_y == 12) {
+	   		reward = -100;
+	   	}
+		if (b->y_pos > SCREEN_HEIGHT-SIZE || b->y_pos < 0 || b->x_pos > SCREEN_WIDTH-SIZE  || b->x_pos < 0) {
+			sendMessage(b, move, (SCREEN_WIDTH/2)-SIZE/2,(SCREEN_HEIGHT/2)-SIZE/2);
+		}
+		
+		// Update our q-values using the Q-learning update rule
+		qvalues[x][y][actionIdx] += alpha * (reward + gamma	 * qvalues[new_x][new_y][new_action] -  qvalues[x][y][actionIdx]);
+		
+		_delay_ms(75);
 	}
 	//cleanup
 	free(b);
 	free(acc);
+}
+
+/* Returns a uniformly random integer in the range [0, n) */
+int randint(int n) {	
+  if ((n - 1) == RAND_MAX) {
+    return rand();
+  } else {
+    // Chop off all of the values that would cause skew...
+    long end = RAND_MAX / n; // truncate skew
+    //assert (end > 0L);
+    end *= n;
+
+    // ... and ignore results from rand() than fall above that limit.
+    // (Worst case the loop condition should succeed 50% of the time, so we can
+    // expect to bail out of this loop pretty quickly.)
+    int r;
+    while ((r = rand()) >= end);
+    return r % n;
+  }
 }
